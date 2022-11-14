54e6f0cd46cd9bd7da4a8b7423a8c6551eaa77e3
raulberna <47525849+raulbr99@users.noreply.github.com>
2022-11-14 15:07:57 +0100

    make more component

--- components/Player/GlobalPlayer.tsx
+++ components/Player/GlobalPlayer.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState }from "react"
+import React, { useEffect, useState, useRef } from "react"
 import AudioPlayerHolder from "./AudioPlayerHolder"
 import BackSkipButton from "./BackSkipButton"
 import ForwardSkipButton from "./ForwardSkipButton"
@@ -10,6 +10,7 @@ import { BiRightArrowAlt } from "react-icons/bi"
 import { BiLeftArrowAlt } from "react-icons/bi"
 import { FaPlay } from "react-icons/fa"
 import { FaPause } from "react-icons/fa"
+import { Progress } from "@material-tailwind/react";
 interface Props {
     postId: string
 }
@@ -20,9 +21,25 @@ interface State {
 const GlobalPlayer = (props: any) => {
     // state
     const [isPlaying, setIsPlaying] = useState(false);
+    const [duration, setDuration] = useState(0);
+    const [currentTime, setCurrentTime] = useState(0);
 
     // references
-    
+    const audioPlayer: any = useRef();
+    const progressBar: any = useRef();
+
+    const togglePlayPause = () => { 
+        const prevValue = isPlaying
+        setIsPlaying(!isPlaying); 
+        
+        if(!prevValue){
+            audioPlayer.current.play();
+        } else {
+            audioPlayer.current.pause();
+        }
+
+    }
+
     useEffect(() => {
         const url = `${process.env.FEED_API_BASE_URL}posts/${props.postId}?api_key=16dea2a1-35e8-4332-8cd6-e534300d16b7`;
         fetch(url, { method: "GET" })
@@ -32,31 +49,52 @@ const GlobalPlayer = (props: any) => {
             .then((data) => {
                 //this.setState(data.data[0])
             })
-    })
+        const seconds = Math.floor(audioPlayer.current.duration)
+        setDuration(seconds)
+    }, [audioPlayer?.current?.loadedmetadata, audioPlayer?.current?.readyState])
 
-    const togglePlayPause = () => { setIsPlaying(!isPlaying); } 
-        return (
-            <div className="md:w-1/3  mx-auto py-10 px-2  bg-gray-400 bg-opacity-30 border border-gray-100 rounded-b-3xl shadow-3xl">
-                <div className="flex flex-row justify-center items-center w-full py-4 px-2">
-                    <audio src="https://d3ctxlq1ktw2nl.cloudfront.net/staging/2022-10-4/295063124-44100-2-967bc9c977063.mp3" preload="metadata" />
-                    <button className="p-3 mx-7 bg-teal-500 rounded-full"><BiLeftArrowAlt size={30}/></button>
-                    <button onClick={togglePlayPause} className="p-3 mx-7 bg-teal-500 rounded-full">
-                        { isPlaying ? <FaPause size={30}/> : <FaPlay size={30}/>}
-                    </button>
-                    <button className="p-3 mx-7 bg-teal-500 rounded-full"><BiRightArrowAlt size={30}/></button>
+    const calculateTime = (secs: number) => {
+        const minutes = Math.floor(secs / 60);
+        const returnMin = minutes < 10 ? `0${minutes}`:`${minutes}`;
+        const seconds = Math.floor(secs % 60);
+        const returnSecs = seconds < 10 ? `0${seconds}`:`${seconds}`;
+        return `${returnMin}:${returnSecs}`
+    }
+
+    const changeRange = () =>{
+        audioPlayer.current.currentTime = progressBar.current.value;
+        //console.log(progressBar.current)
+        //progressBar.current.value = progressBar.current.value
+        //setCurrentTime(progressBar.current.value)
+    }
+
+    return (
+        <div className="md:w-1/3  mx-auto py-10 px-2  bg-gray-400 bg-opacity-30 border border-gray-100 rounded-b-3xl shadow-3xl">
+            <div className="flex flex-row justify-center items-center w-full py-4 px-2">
+                <audio ref={audioPlayer} src="https://d3ctxlq1ktw2nl.cloudfront.net/staging/2022-10-4/295063124-44100-2-967bc9c977063.mp3" preload="metadata" />
+                <button className="p-3 mx-7 bg-teal-500 rounded-full"><BiLeftArrowAlt size={30} /></button>
+                <button onClick={togglePlayPause} className="p-3 mx-7 bg-teal-500 rounded-full">
+                    {isPlaying ? <FaPause size={30} /> : <FaPlay size={30} />}
+                </button>
+                <button className="p-3 mx-7 bg-teal-500 rounded-full"><BiRightArrowAlt size={30} /></button>
+            </div>
+            <div className="flex flex-row justify-center font-mono items-center w-full py-4 px-2">
+                {/* current time */}
+                <div className="p-1 mx-7 bg-teal-500 rounded-md">
+                    {calculateTime(currentTime)}
                 </div>
-                <div className="flex flex-row justify-center font-mono items-center w-full py-4 px-2">
-                    {/* current time */}
-                    <div className="p-1 mx-7 bg-teal-500 rounded-md">0:00</div>
 
-                    {/* progress bar */}
-                    <div className="p-1 mx-7">
-                        <input id="default-range" type="range" value="0" className="overflow-hidden appearance-none bg-gray-400 h-3 w-full"/>
-                    </div>
-                    {/* duration */}
-                    <div className="p-1 mx-7 bg-teal-500 rounded-md">48:00</div>
+                {/* progress bar */}
+                <div className="w-full bg-gray-200 rounded-full h-2.5 dark:bg-gray-700">
+                    <div ref={progressBar} onChange={changeRange} className="bg-teal-500 h-2.5 rounded-full" style={{ width: `0%`}} ></div>
+                </div>
+
+                {/* duration */}
+                <div className="p-1 mx-7 bg-teal-500 rounded-md">
+                    {(duration && !isNaN(duration)) && calculateTime(duration)}
                 </div>
             </div>
-        )
+        </div>
+    );
 }
 export { GlobalPlayer };
