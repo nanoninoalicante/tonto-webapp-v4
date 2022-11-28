0b33cd30bde2a5202e8f302bf6560dbc4c7608e3
raulberna <47525849+raulbr99@users.noreply.github.com>
2022-11-28 13:06:15 +0100

    Update [post].tsx

--- pages/post/[post].tsx
+++ pages/post/[post].tsx
@@ -16,7 +16,7 @@ type State = {
 //santeetji: 62b131b4db1ec8000f04084e
 //begaes: 628e108820eaae000f00a887
 //raul: 6381ce9059b930327afece05
-const slug = "628e108820eaae000f00a887"
+const slug = "6381ce9059b930327afece05"
 const Post = () => {
   /* const router = useRouter()
   const { slug } = router.query */
