a6e33fb1fd093f594df491882de88e42d640d7bf
raulberna <47525849+raulbr99@users.noreply.github.com>
2022-12-19 12:14:54 +0100

    try

--- next.config.js
+++ next.config.js
@@ -1,9 +1,16 @@
 /** @type {import('next').NextConfig} */
+const cors = require('cors');
+const nextConnect = require('next-connect');
 
 const nextConfig = {
   //reactStrictMode: true,
   swcMinify: true,
 }
+
+module.exports = nextConnect().use(cors({
+  origin: 'origin',
+  optionsSuccessStatus: 200
+}));
 
 module.exports = {
   webpack(config) {
