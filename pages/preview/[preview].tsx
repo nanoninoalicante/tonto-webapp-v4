faa0a115b0d26ea9c897e6bcab82b33ac7e4655f
raulberna <47525849+raulbr99@users.noreply.github.com>
2023-02-15 10:34:57 +0100

    Update [preview].tsx

--- pages/preview/[preview].tsx
+++ pages/preview/[preview].tsx
@@ -43,29 +43,36 @@ const Preview = (props: any) => {
     if (!profileImg) profileImg = "/flex-ui-assets/images/tontoprofile_defualt.png";
     const { description } = props.data
     return (
-        <div className="flex flex-col">
+        <div className="flex flex-col h-screen bg-white text-black">
             <MetaTags data={props.data} />
             <div className="top-0 inset-x-0 w-full">
                 <img className="w-full" src={profileImg}></img>
             </div>
-            <section className="px-3 mt-10">
-                <p className="text-xs font-bold">
-                    TONTO
-                </p>
-                <p className="text-xl ">
-                    {userName}
-                </p>
-                <p>
-                    {description}
-                </p>
-            </section>
-            <section className="flex mt-2">
-                <Icon className="rounded-xl ml-2 h-[56px]"/>
-                <Link href={"https://app.gettonto.com/download"} 
-                      className='bg-gray-800 h-[56px] text-white px-10 py-4 ml-auto mr-2 w-3/4 flex justify-center items-center'>
-                    Get The App
-                </Link>
-
+            <section className="mx-3">
+                <div className="mt-10">
+                    <p className="text-xs font-bold">
+                        TONTO
+                    </p>
+                    <p className="text-xl ">
+                        {userName}
+                    </p>
+                    <p>
+                        {description}
+                    </p>
+                </div>
+                <div className="flex flex-col mt-2">
+                    <div className="flex flex-col items-center leading-2 text-sm font-thin justify-center">
+                        See this content in the app
+                        <div className="w-full h-[2px] bg-gray-400 "/>
+                    </div>
+                    <div className="flex flex-row mt-2">
+                        <Icon className="rounded-xl ml-2 h-[56px]"/>
+                        <Link href={"https://app.gettonto.com/download"} 
+                            className='bg-gray-800 h-[56px] text-white px-10 py-4 ml-auto w-3/4 flex justify-center items-center'>
+                            Get The App
+                        </Link>
+                    </div>
+                </div>
             </section>
         </div>
     )
