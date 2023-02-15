be1d41266c9e5957b4429bf39de6b6bf5d43eb96
raulberna <47525849+raulbr99@users.noreply.github.com>
2023-02-15 12:30:00 +0100

    small banner just in android/iOS

--- pages/preview/[preview].tsx
+++ pages/preview/[preview].tsx
@@ -20,8 +20,15 @@ const previewData = {
 }
 export const getServerSideProps = async (context: any) => {
     const { preview } = context.query;
+    const { req } = context;
+    const userAgent = req.headers["user-agent"];
+    let isPhone: boolean = false;
+    if (userAgent.indexOf("iPhone") !== -1 || userAgent.indexOf("Android") !== -1) {
+        isPhone = true
+    }
     let server = {
-        data: previewData
+        data: previewData,
+        isPhone: isPhone
     }
 
     const getPost = `${process.env.FEED_API}/post/${preview}${process.env.API_KEY}`;
@@ -38,13 +45,27 @@ export const getServerSideProps = async (context: any) => {
 };
 
 const Preview = (props: any) => {
+    const { isPhone } = props
+    const { description } = props.data
     let { profileImg, userName } = props.data.userInfo;
     if (!profileImg) profileImg = "/flex-ui-assets/images/tontoprofile_defualt.png";
-    const { description } = props.data
     return (
         <div className="flex flex-col h-screen bg-white text-black md:justify-center md:items-center md:relative">
             <MetaTags data={props.data} />
-            <div className="top-0 inset-x-0 w-full md:w-1/3">
+            { isPhone && 
+                <div className="relative w-full h-20 bg-white flex flex-row items-center">
+                    <Icon className="mx-3 w-15 h-15 rounded-full"/>
+                    <div className="flex flex-col">
+                        <div className="text-lg font-medium"> Tonto - Social Audio App</div>
+                        <div className="text-sm font-light leading-3"> Open in Tonto App</div>
+                    </div>
+                    <Link href={"https://app.gettonto.com/download"}
+                          className="bg-[#109C90] text-white ml-auto rounded-2xl px-3 py-1 mr-3 h-8 flex items-center">
+                        OPEN
+                    </Link>
+                </div>
+            }
+            <div className="relative inset-x-0 w-full md:w-1/3">
                 <img className="w-full" src={profileImg}></img>
             </div>
             <section className="mx-3 md:w-1/3 md:px-2">
@@ -62,12 +83,12 @@ const Preview = (props: any) => {
                 <div className="flex flex-col mt-2">
                     <div className="flex flex-col items-center leading-2 text-sm font-thin justify-center">
                         See this content in the app
-                        <div className="w-full h-[2px] bg-gray-400 "/>
+                        <div className="w-full h-[2px] bg-gray-400 " />
                     </div>
                     <div className="flex flex-row mt-2">
-                        <Icon className="rounded-xl ml-2 h-[56px]"/>
-                        <Link href={"https://app.gettonto.com/download"} 
-                            className='bg-gray-800 h-[56px] md:w-[85%] text-white px-10 py-4 ml-auto w-3/4 flex justify-center items-center'>
+                        <Icon className="rounded-xl ml-2 h-[56px]" />
+                        <Link href={"https://app.gettonto.com/download"}
+                            className='bg-[#109C90] h-[56px] md:w-[85%] text-white px-10 py-4 ml-auto w-3/4 rounded-xl flex justify-center items-center'>
                             Get The App
                         </Link>
                     </div>
@@ -76,5 +97,6 @@ const Preview = (props: any) => {
         </div>
     )
 }
+
 
 export default Preview;
