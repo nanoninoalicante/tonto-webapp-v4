e785be64fb477c6dfa7254b677efa3c65206db46
raulberna <47525849+raulbr99@users.noreply.github.com>
2023-04-25 13:26:32 +0200

    Update [profile].tsx

--- pages/profile/[profile].tsx
+++ pages/profile/[profile].tsx
@@ -24,12 +24,15 @@ export const getServerSideProps = async (context: any) => {
 }
 
 const Profile = (props: any) => {
+    const image = props.profileImg || "/flex-ui-assets/images/tontoprofile_defualt.png";
     return (
         <main className="grid place-items-center relative md:top-[10vh]">
             <section className="w-full flex flex-col items-center justify-center font-medium ">
                 <PrimaryHeader />
                 <div className="flex flex-col justify-center items-center mt-10">
-                    <img src={props.profileImg} className="w-[300px] rounded-full" alt="Profile" />
+                    <div className="relative inset-x-0 w-full md:w-1/3">
+                        <img loading="lazy" className="w-full max-h-96 md:max-h-full" src={image}></img>
+                    </div>
                     <div className="text-3xl font-bold mt-2">{props.userName}</div>
                     <Link href={`${props.link}`} className="bg-teal-500/70 hover:bg-teal-600 dark:hover:bg-white dark:hover:text-teal-500  text-white rounded-[5px] py-3 px-4 mt-10"> See in the app </Link>
                 </div>
