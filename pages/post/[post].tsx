f0762f3f529cd518e741bcbb5932e1b80d9dee8b
raulberna <47525849+raulbr99@users.noreply.github.com>
2023-10-23 13:46:11 +0200

    Update [post].tsx

--- pages/post/[post].tsx
+++ pages/post/[post].tsx
@@ -68,7 +68,8 @@ export const getServerSideProps = async (context: any) => {
     server.firebase = postFirestore;
     const dataPost = await getPost(post)
     server.data = dataPost;
-    console.log(server.data)
+    console.log("post data: ", dataPost)
+    console.log("server data: ", server.data)
     const getUser = await getUserInfo(server.data.userInfo.id, post)
     const getComment = await getCommentsByUser(post)
     server.back = getUser.back;
